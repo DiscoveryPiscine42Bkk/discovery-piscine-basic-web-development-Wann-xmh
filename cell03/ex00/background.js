b127e86232cafaed1c1747def244c48aa5224436
Sasitorn Jaroenpompong <sajaroen@z2t14c4.42bangkok.com>
2025-04-30 10:13:04 +0700

    add file

---
+++ cell03/ex00/background.js
@@ -0,0 +1,15 @@
+function Change_color(){
+    var a = Math.floor(Math.random()*255)+1;
+    var b = Math.floor(Math.random()*255)+1;
+    var c = Math.floor(Math.random()*255)+1;
+    // alert(1);
+    document.body.style.background= "rgb(" + a + "," + b + "," + c + ")";
+}
+
+// function random_color(){
+//     let a = Math.floor(Math.random()*255)+1;
+//     let b = Math.floor(Math.random()*255)+1;
+//     let c = Math.floor(Math.random()*255)+1;
+//     return rgb(a,b,c);
+
+// }
