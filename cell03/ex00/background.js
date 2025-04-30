e0710a65a6731b1a7c8573b7d3aff20f01182a7e
Sasitorn Jaroenpompong <sajaroen@z2t14c4.42bangkok.com>
2025-04-30 17:37:10 +0700

    add file

--- cell03/ex00/background.js
+++ cell03/ex00/background.js
@@ -1,7 +1,7 @@
 function Change_color(){
-    var a = Math.floor(Math.random()*255)+1;
-    var b = Math.floor(Math.random()*255)+1;
-    var c = Math.floor(Math.random()*255)+1;
+    var a = Math.floor(Math.random()*256);
+    var b = Math.floor(Math.random()*256)
+    var c = Math.floor(Math.random()*256);
     // alert(1);
     document.body.style.background= "rgb(" + a + "," + b + "," + c + ")";
 }
